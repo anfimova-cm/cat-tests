35ffd826da0a6a89d2210ee9cfa07d6de64b6775
a.anfimova@cardsmobile.ru <a.anfimova@cardsmobile.ru>
2020-02-21 18:58:30 +0300

    DataGenerator

--- src/test/java/cat_tests/tests/Notes.java
+++ src/test/java/cat_tests/tests/Notes.java
@@ -1,38 +1,17 @@
 package cat_tests.tests;
 
+import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Notes {
 
     /*
-
-    ChromeOptions co = new ChromeOptions();
-    co.addArguments("--start-maximized");
-    driver = new ChromeDriver(co);
-
     By.xpath("//a[contains(text(),'часть текста')]")
     By.xpath("//button[contains(@class,'btn-text') and contains(text(), 'Добавить')]")
-
-    ---------------
-
-    example:
-    JavascriptExecutor js = (JavascriptExecutor) driver;
-    js.executeScript(Script,Arguments);
-
-    js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-
-    WebElement element = driver.findElement(By.xpath("//input [@id='giveid']"));
-    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();". element);
-    or
-    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true)", element);
-    Thread.sleep(5); or sleep(5);
-
-    var element = Driver.findElement(By.Id("element-id"));
-    Actions actions = new Actions(driver);
-    actions.moveToElement(element).perform();
-
-    ---------------
-
-    Сравнивать HashSet - неупорядоченные, List - упорядоченные (одинаковые схлопываются)
-    Assert.assertNotEquals(new HashSet<Object>(beforeAll), new HashSet<Object>(afterRename));
 
     ---------------
 
